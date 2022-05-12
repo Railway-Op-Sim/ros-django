00b198bb3c6f80cf73f3b8538e5fc891f58cf5d4
Daniel-Gill <d.gill@se20.qmul.ac.uk>
2022-05-12 23:27:08 +0100

    Moved security key

--- rospy/settings.py
+++ rospy/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+from rospy import security
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# key removed
+SECRET_KEY = security.key
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
